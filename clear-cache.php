038da7cddc38877d5be43bf4e2186e46058f1629
Mike Kilmer <mrmikekilmer@gmail.com>
2016-01-21 19:27:13 -0500

    Update cache

--- clear-cache.php
+++ clear-cache.php
@@ -1,6 +1,9 @@
 <?php
 
-    clearstatcache();
-    drupal_theme_rebuild();
+function drupal_theme_rebuild() {
+    drupal_static_reset('theme_get_registry');
+    cache_clear_all('theme_registry', 'cache', TRUE);
+}
 
+drupal_theme_rebuild();
 ?>
